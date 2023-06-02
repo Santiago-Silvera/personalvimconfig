674e00cc568d5e01acdf3b7f814211c450f513d9
Santiago-Silvera <santiagosil3434@gmail.com>
2023-06-02 01:31:36 -0300

    Removed lualine

--- lua/santiago-silvera/packer.lua
+++ lua/santiago-silvera/packer.lua
@@ -47,10 +47,6 @@ return require('packer').startup(function(use)
         {'L3MON4D3/LuaSnip'},     -- Required
     },
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    },
     use "lukas-reineke/indent-blankline.nvim",
     use "nvim-lua/plenary.nvim",
     use {
